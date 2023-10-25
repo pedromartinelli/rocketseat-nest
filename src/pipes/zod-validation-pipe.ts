fb97d2cb842693cdd4dfb24b12075c91e20ff5f4
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-10-25 10:54:54 -0300

    {

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,5 +1,6 @@
 import { PipeTransform, BadRequestException } from '@nestjs/common';
 import { ZodError, ZodSchema } from 'zod';
+import { fromZodError } from 'zod-validation-error';
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -10,9 +11,8 @@ export class ZodValidationPipe implements PipeTransform {
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException({
-          message: 'Validation failed',
           statusCode: 400,
-          errors: error.format(),
+          errors: fromZodError(error),
         });
       }
       throw new BadRequestException('Validation failed');
